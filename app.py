328a6c34f7d2f01ed25210c7e4571ee310a63acd
Jyotsna <jyotsnabh03@gmail.com>
2022-06-07 12:07:59 +0530

    changes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+import typing
 import time
 import datetime
 
@@ -12,6 +13,18 @@ app = Flask(__name__)
 @app.route('/test', methods=['GET'])
 def helloworld():
     return jsonify({"data": "New Hello World", "request_id": unix_time}), 200
+
+
+@app.route('/', methods=['GET'])
+def helloworld():
+    return jsonify({"data": "Hello Jyotsna ", "request_id": unix_time}), 200
+
+
+@app.get("/auth")
+def read_item(api_key: typing.Union[str, None] = None):
+    if api_key == '1234':
+        return {'status': 'OK'}, 200
+    return {'status':''}, 210
 
 
 if __name__ == '__main__':
